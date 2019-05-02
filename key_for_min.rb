8197cfae35fa38eb3097dbec812bc1f3ed3f9bf0
Toni Lamont <tlam212@gmail.com>
2019-05-02 15:11:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   else
     min_value = name_hash.first[1]
     min_key = name_hash.first[0]
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
   if value < min_value
     min_value = value
     min_key = key
